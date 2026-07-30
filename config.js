2310f9e171b534e9e2ff15069a2df2d9c9cad6a8
ayanali7860 <mptlu75@gmail.com>
2026-07-30 12:32:29 +0500

    Update config.js

--- config.js
+++ config.js
@@ -43,7 +43,7 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "923005448162",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "923242895504",
 // add your bot owner number
 OWNER_NAME: process.env.OWNER_NAME || "SARWAR-MD",
 // add bot owner name
@@ -75,7 +75,7 @@ AUTO_TYPING: process.env.AUTO_TYPING || "false",
 // true for automatic show typing   
 READ_CMD: process.env.READ_CMD || "false",
 // true if want mark commands as read 
-DEV: process.env.DEV || "923005448162",
+DEV: process.env.DEV || "923242895504",
 //replace with your whatsapp number        
 ANTI_VV: process.env.ANTI_VV || "true",
 // true for anti once view 
